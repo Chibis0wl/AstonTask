5bb03f240ab2fcc16a6e5185b0484ca2287fd7d1
Chibis0wl <165270582+Chibis0wl@users.noreply.github.com>
2026-05-10 20:39:21 +0300

    Update TXT.java

--- src/main/java/TXT.java
+++ src/main/java/TXT.java
@@ -4,7 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class TXT {
+public class Main
+{
 
     public static void main(String[] args) throws IOException {
         Task.numsOperations();
