40067c78e0d27d5ba57fb178d510149f0fe7e7b2
Chibis0wl <165270582+Chibis0wl@users.noreply.github.com>
2026-05-10 20:40:01 +0300

    Update Task.java

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Task {
+public class Utils {
 
     private static int num1;
     private static int num2;
